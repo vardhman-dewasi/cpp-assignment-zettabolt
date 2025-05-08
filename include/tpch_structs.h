b24fe63e24d050e738e3df528cd913a07bdee6f2
vardhman dewasi <vardhman738@gmail.com>
2025-05-08 18:52:49 +0530

    Created tpch_structs header

---
+++ include/tpch_structs.h
@@ -0,0 +1,40 @@
+#ifndef TPCH_STRUCTS_H
+#define TPCH_STRUCTS_H
+
+#include <string>
+
+struct Region {
+    int r_regionkey;
+    std::string r_name;
+};
+
+struct Nation {
+    int n_nationkey;
+    std::string n_name;
+    int n_regionkey;
+};
+
+struct Customer {
+    int c_custkey;
+    int c_nationkey;
+};
+
+struct Order {
+    int o_orderkey;
+    int o_custkey;
+    std::string o_orderdate;
+};
+
+struct LineItem {
+    int l_orderkey;
+    int l_suppkey;
+    double l_extendedprice;
+    double l_discount;
+};
+
+struct Supplier {
+    int s_suppkey;
+    int s_nationkey;
+};
+
+#endif // TPCH_STRUCTS_H
